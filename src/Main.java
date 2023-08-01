d7d78b06975c857526f47f09655ead26be324ee8
rmcampigotto <rmcampigotto@gmail.com>
2023-08-01 19:49:47 -0300

    JOptionPaneAdd0801

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 import Aulas.Aula1;
+import javax.swing.JOptionPane;
+import java.lang.Object;
 
 public class Main {
     public static void main(String[] args) {
-        double[] vet = {10,20,30};
+
+        double[] vet = {10, 20, 30};
         Aula1 aula = new Aula1();
 
         double media = aula.caucularMedia(vet);
 
-        System.out.println(media);
+        JOptionPane.showMessageDialog(null, "MEDIA: " + media, "MEDIA", 1);
 
         if (media != 20){
-            System.out.println("Falha! O resultado da média está incorreto!");
+            JOptionPane.showMessageDialog(null,"Falha! O resultado da média está incorreto!", "MEDIA", 1);
         } else {
-            System.out.println("Teste passou! A média está correta");
+            JOptionPane.showMessageDialog(null, "Teste passou! A média está correta", "MEDIA", 1);
         }
 
     }
