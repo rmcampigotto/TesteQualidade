cc644f530e63a61abd029d2ff6e5e1aba876f6c1
rmcampigotto <rmcampigotto@gmail.com>
2023-08-01 20:31:04 -0300

    JOptionPaneAlteration0801

--- src/Aulas/Aula1/MainAula1.java
+++ src/Aulas/Aula1/MainAula1.java
@@ -1,11 +1,11 @@
 package Aulas.Aula1;
-
 import javax.swing.JOptionPane;
+import static java.lang.Integer.parseInt;
 
 public class MainAula1 {
     public static void main(String[] args) {
 
-        double[] vet = {10, 20, 30};
+        double[] vet = {parseInt(JOptionPane.showInputDialog("Insira o valor 1")),parseInt(JOptionPane.showInputDialog("Insira o valor 2")),parseInt(JOptionPane.showInputDialog("Insira o valor 3"))};
         Aula1 aula = new Aula1();
 
         double media = aula.caucularMedia(vet);
